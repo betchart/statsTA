f505926e9ffcdb4fffe61a1bdf558e1f221384c6
Burton Betchart <betchart@gmail.com>
2013-03-12 16:43:36 -0400

    generalize inputs to other distributions, like rapidity

--- inputs.py
+++ inputs.py
@@ -1,9 +1,5 @@
 import ROOT as r,utils
 
-# can symmetrize:
-#  DY
-#  QCD
-
 class sample_data(object) :
     def __init__(self, signalDistributions, xs = None, sigma = None, selectionEfficiency = 1.0, preselectionFraction = 1.0 ) :
         self.xs = xs
@@ -15,6 +11,7 @@ class sample_data(object) :
         for d in filter(None,self.datas) :
             #d.Scale(1./norm)
             d.RebinY(20)
+            if 'QueuedBin' not in d.GetName() : d.RebinX()
         
         self.datasX = tuple(d.ProjectionX() if d else None for d in self.datas)
         self.datasY = tuple(d.ProjectionY() if d else None for d in self.datas)
@@ -28,12 +25,13 @@ class sample_data(object) :
     def key(self) : return ( self.xs, self.frac )
 
 class channel_data(object) :
-    __samples__ = ['wj','dy','st','ttgg','ttqg','ttqq','ttag','qcd','data']
+    __samples__ = ['wj','dy','st','ttgg','ttqg','ttqq','ttag','qcd','data','tt']
     __xs_uncertainty__ = {'tt':1.0,'wj':2.0,'st':0.04,'dy':0.04}
 
     def __init__(self,lepton, filePattern="data/stats_melded_%s_ph_pn_sn_jn_20.root",
                  signal="fitTopQueuedBin7TridiscriminantWTopQCD", 
-                 preselection="allweighted/" ) :
+                 preselection="allweighted/",
+                 getTT = False) :
         tfile = r.TFile.Open(filePattern%lepton)
 
         self.lepton = lepton
@@ -41,11 +39,12 @@ class channel_data(object) :
         self.lumi_sigma = 0.04
         self.samples = {}
 
-        for s in self.__samples__:
+        for s in self.__samples__[:None if getTT else -1]:
             pre = tfile.Get(preselection+s)
+            doSymmAnti = s[:2]=='tt' and 'QueuedBin' in signal
             datas = ( tfile.Get(signal+     '/'+s).Clone(lepton+'_'+s),
-                      tfile.Get(signal+'_symm/'+s).Clone(lepton+'_symm_'+s) if s[:2]=='tt' else None,
-                      tfile.Get(signal+'_anti/'+s).Clone(lepton+'_anti_'+s) if s[:2]=='tt' else None)
+                      tfile.Get(signal+'_symm/'+s).Clone(lepton+'_symm_'+s) if doSymmAnti else None,
+                      tfile.Get(signal+'_anti/'+s).Clone(lepton+'_anti_'+s) if doSymmAnti else None)
             for d in filter(None,datas) : d.SetDirectory(0)
             xs = tfile.Get('xsHisto/'+s).GetBinContent(1) if s!='data' else None
             delta = self.__xs_uncertainty__[s[:2]] if s[:2] in self.__xs_uncertainty__ else None
