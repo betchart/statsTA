a791811b03887a22778ac868a05892fb6592776a
Burton Betchart <betchart@gmail.com>
2013-04-12 00:27:15 -0400

    lots of changes; iterate and record systematics

--- start.py
+++ start.py
@@ -12,12 +12,14 @@ import systematics
 from falphaLSlice import falphaLSlice
 from paraboloid import paraboloid
 from parabola import parabola
+from itertools import combinations_with_replacement
 
 class fit(object):
     def __init__(self, label, signal, profileVars, R0_,
                  d_lumi, d_xs_dy, d_xs_st, tag, genPre, sigPre, dirIncrement, quiet = False,
                  hackZeroBins=False):
 
+        self.label = label
         self.quiet = quiet
         self.profileVars = profileVars
         self.doAsymm = 'QueuedBin' in signal
@@ -38,7 +40,7 @@ class fit(object):
                                               sigPrefix = sigPre if not dirIncrement else '',
                                               dirPrefix="R01", getTT=True,
                                               prePre = not dirIncrement)
-        
+
         if diffR0_ :
             for lepPart,chan in channels.items():
                 if type(lepPart) != tuple: continue
@@ -48,61 +50,76 @@ class fit(object):
 
         print "###", label
         self.model = model.topModel(channels, asymmetry=self.doAsymm, quiet=True)
-        for item in ['d_lumi', 'd_xs_dy', 'd_xs_st']:
-            self.model.w.arg(item).setVal(eval(item))
+        for item in ['d_lumi', 'd_xs_dy', 'd_xs_st']: self.model.w.arg(item).setVal(eval(item))
         self.model.import_data()
         self.fitArgs = [r.RooFit.Extended(True), r.RooFit.NumCPU(1),
-                        r.RooFit.PrintLevel(-1), r.RooFit.Save()]
+                        r.RooFit.PrintLevel(-1)]
 
-        self.doCentral()
-
-    @roo.quiet
-    def doCentral(self):
-        w = self.model.w
-        self.central = w.pdf('model').fitTo(w.data('data'), *self.fitArgs)
-        if not self.quiet:
-            self.central.Print()
-            w.arg('d_qq').Print()
-            if self.doAsymm: w.arg('alphaT').Print()
+        self.doFit()
 
     @roo.quiet
-    def doProfile(self):
+    def doFit(self):
         w = self.model.w
-        meanSigs = [(w.arg(a).getVal(),w.arg(a).getError()) for a in self.profileVars]
+        N = len(self.profileVars)
 
-        nll = w.pdf('model').createNLL(w.data('data'), *self.fitArgs[:-2])
+        nll = w.pdf('model').createNLL(w.data('data'), *self.fitArgs[:-1])
+        minu = r.RooMinuit(nll)
+        minu.setPrintLevel(-1)
+        minu.setNoWarn()
+        for i in range(100):
+            print i
+            if not minu.migrad(): break
+        print minu.improve()
+        print minu.minos(w.argSet(','.join(self.profileVars)))
         pll = nll.createProfile(w.argSet(','.join(self.profileVars)))
 
-        def point(*vals):
-            for name,val in zip(self.profileVars,vals) :
-                w.arg(name).setVal(val)
-            return tuple(vals) + (pll.getVal(),)
+        def pllEval(**kwargs) :
+            for name,val in kwargs.items(): w.arg(name).setVal(val)
+            return pll.getVal()
 
-        if not self.doAsymm:
-            mean,sigma = meanSigs[0]
-            points = [point(*p) for p in [(mean,),(mean+sigma,),(mean-sigma,)]]
+        def vE(a) : return w.arg(a).getVal(), w.arg(a).getError()
+
+        muSig = [vE(a) for a in self.profileVars]
+        points = [p + (pllEval(**dict(zip(self.profileVars, p))),) 
+                  for p in [tuple(m + i*s for i, (m, s) in zip(signs, muSig))
+                            for signs in combinations_with_replacement([0,-1, 1], N)]]
+        print zip(*points)[2]
+
+        oneSigmaNLL = {1: 0.5, 2: 1.14}[N]
+        if N == 1:
             parb = parabola(points)
-            oneSigmaNLL = 0.5
-            oneSigmas = parb.dx(oneSigmaNLL)
-            print oneSigmas
-            print parb.xmin
-            return
+            self.profVal = parb.xmin,
+            self.profErr = parb.dx(oneSigmaNLL)
+        else:
+            parb = paraboloid(points)
+            self.profVal = parb.xymin
+            self.profErr = parb.dxy(oneSigmaNLL)
 
-        (faL,faLe),(faT,faTe) = meanSigs
-        points = [point(faL + dfaL, faT + dfaT) for dfaL, dfaT in
-                  [(0, 0), (-faLe, 0), (faLe, 0), (0, -faTe), (0, faTe), (-faLe, faTe)]]
+        if self.doAsymm: 
+            self.scales = np.array([w.arg(a).getVal() for a in ['Ac_y_ttqq', 'Ac_y_ttqg']])
+            self.scalesPhi= [w.arg('Ac_phi_%s'%n) for n in ['ttqq','ttgg','ttag','ttqg','tt']]
 
-        print '\n'.join(str(p) for p in points)
+        self.parb = parb
+        self.muSig = muSig
+        self.profPLL = pllEval(**dict(zip(self.profileVars,self.profVal)))
+        self.fitPLL = pllEval(**dict(zip(self.profileVars,[m for m,_ in muSig])))
+
+        if not self.quiet:
+            print zip(*muSig)[0], self.fitPLL
+            print self.profVal, self.profPLL
+            print self.profErr
+            for item in ['d_qq','d_xs_tt','d_xs_wj','factor_elqcd','factor_muqcd']:
+                print '\t',
+                w.arg(item).Print()
+        return
+
         parb = paraboloid(points)
-        oneSigmaNLL = 1.14
-        twoSigmaNLL = 3.0
         oneSigmas = parb.dxy(oneSigmaNLL)
         print oneSigmas
         print math.sqrt(oneSigmas[0,0]), math.sqrt(oneSigmas[1,1])
         print parb.xymin
         param1sigma = parb.parametricEllipse(oneSigmaNLL)
         param2sigma = parb.parametricEllipse(twoSigmaNLL)
-        scales = [w.arg(a).getVal() for a in ['Ac_y_ttqq', 'Ac_y_ttqg']]
         with open('stat.txt', 'w') as wfile:
             for t in np.arange(0, 2 * math.pi + 0.00001, math.pi / 50):
                 point1 = param1sigma.dot([math.cos(t), math.sin(t), 1])
@@ -113,22 +130,39 @@ class fit(object):
                 print>>wfile, '\t'.join(str(f) for f in seq)
         print "Wrote stat.txt"
 
+    @classmethod
+    def fields(cls): return '#label Ac_y_qq  Ac_y_qg  XX  XY  YY'
+
+    def __str__(self):
+        return '\t'.join(str(i) for i in [self.label] + list(self.profVal*self.scales) + [self.profErr[0,0]*self.scales[0]**2,
+                                                                                          self.profErr[0,1]*self.scales[0]*self.scales[1],
+                                                                                          self.profErr[1,1]*self.scales[1]**2])
 
 class measurement(object):
     def __init__(self, label, signal, profile, R0_, hackZeroBins=False):
+        write = open('data/' + '_'.join(label.split(',')) + '.txt', 'w')
+        print >> write, fit.fields()
+
         self.central = fit(signal=signal, profileVars=profile, R0_=R0_,
                            hackZeroBins=hackZeroBins, **systematics.central())
-        self.central.doProfile()
-        return
+
+        print >> write, str(self.central)
+
         syss = []
         for sys in systematics.systematics():
             pars = systematics.central()
             pars.update(sys)
-            f = fit(signal=signal, profile=profile, R0_=R0_, quiet=True, **pars)
-            try: pass
-            except: print 'failed', pars['label']
-            else: syss.append(f)
+            try:
+                f = fit(signal=signal, profileVars=profile, R0_=R0_, quiet=True, **pars)
+                syss.append(f)
+                print zip(*f.muSig)[0], f.fitPLL
+                print f.profVal, f.profPLL
+                print >> write, str(f)
+            except:
+                print >> write, '#', label, "FAIL"
+            write.flush()
 
+        write.close()
 
 def query(items, default = ()):
     display = '\n'.join('%d %s' % iD for iD in enumerate(items)) + '\nWhich? '
