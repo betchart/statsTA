b8571ef081eea5b72fec5d3a94908930c648f575
Burton Betchart <betchart@gmail.com>
2013-04-18 17:12:39 -0400

    elliptize systematics on simulation prediction (turns out irrelevant)

--- elliptize.py
+++ elliptize.py
@@ -12,14 +12,27 @@ if __name__ == '__main__':
     
     with open(sys.argv[1]) as mFile:
         M = dict([(line.split()[0], tuple(eval(f) for f in line.split()[1:6]))
-                             for line in mFile.readlines() if '#' not in line])
+                  for line in mFile.readlines() if '#' not in line])
+
+
+    with open(sys.argv[1]) as mFile:
+        fields = mFile.readline()[1:].split()
+        hats = dict([(line.split()[0], dict((field,eval(f)) for field,f in zip(fields[6:],line.split()[6:])))
+                     for line in mFile.readlines() if '#' not in line])
+
+    for h,v in hats.items():
+        v['f_qq.Ac_y_qq'] = v['fhat_qq'] * v['Ac_y_qq_hat']
+        v['f_qg.Ac_y_qq'] = v['fhat_qg'] * v['Ac_y_qg_hat']
     
     form = '\t%.8f'*5
     def deltas(k): return [M['central'][i] - M[k][i] for i in [0,1]]
+    def deltasHat(h): return [hats['central'][s] - hats[h][s]
+                              for s in ['f_qq.Ac_y_qq','f_qg.Ac_y_qq']]
     
     def distance(k): return math.sqrt( np.dot(*(2*[deltas(k)])) )
 
     def sigmas(k): return np.outer(*(2*[deltas(k)]))
+    def sigmasHat(h): return np.outer(*(2*[deltasHat(h)]))
 
     def angle(k): return math.atan2(*reversed(deltas(k)))
 
@@ -32,16 +45,19 @@ if __name__ == '__main__':
     stat_sigmas2 = [[central[2],central[3]],
                     [central[3],central[4]]]
     sys_sigmas2 = np.sum(sigmas(k) for k in M) / 2
+    sim_sigmas2 = np.sum(sigmasHat(h) for h in hats) / 2
     
     stat = ellipse(mean=mean, sigmas2=stat_sigmas2)
     syst = ellipse(mean=mean, sigmas2=sys_sigmas2)
     totl = ellipse(mean=mean, sigmas2=(stat_sigmas2+sys_sigmas2))
+    simu = ellipse(mean=(hats['central']['f_qq.Ac_y_qq'],
+                         hats['central']['f_qg.Ac_y_qq']),
+                   sigmas2=sim_sigmas2) if np.all(sim_sigmas2) else None
 
     with open('_points.'.join(sys.argv[1].split('.')), 'w') as wFile:
         N = 100
         for t in range(N + 1):
             T = t * 2 * math.pi / N
-            print >> wFile, '\t'.join(str(f) for f in 
-                                      stat.eval(T) + 
-                                      syst.eval(T) + 
-                                      totl.eval(T))
+            print >> wFile, '\t'.join(str(f) for f in
+                                      sum((getattr(item,'eval')(T)
+                                           for item in filter(None, [stat,syst,totl,simu])), ()))
--- plot.sh
+++ plot.sh
@@ -8,42 +8,42 @@ set ylabel 'f_{qg} A_{c}^{y(qg)}'
 
 
 
-plot [-0.04:0.04] [-0.007:0.007] \
+plot [-0.03:0.03] [-0.007:0.007] \
     'data/asymmetry_full.txt' u 2:3 pt 6 lc 7 notitle, \
     'data/asymmetry_full_points.txt' w lines lt 4 lc 7 notitle, \
     '' u 3:4 w lines lt 2 lc 7 notitle, \
     '' u 5:6 w lines lt 1 lw 3 lc 7 title 'full', \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 notitle
 
-plot [-0.04:0.04] [-0.007:0.007] \
+plot [-0.03:0.03] [-0.007:0.007] \
     'data/asymmetry_hiM.txt' u 2:3 pt 6 lc 5 notitle, \
     'data/asymmetry_hiM_points.txt' w lines lt 4 lc 5 notitle, \
     '' u 3:4 w lines lt 2 lc 5 notitle, \
     '' u 5:6 w lines lt 1 lw 3 lc 5 title 'mass>450GeV', \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 notitle
 
-plot [-0.04:0.04] [-0.007:0.007] \
+plot [-0.03:0.03] [-0.007:0.007] \
     'data/asymmetry_loM.txt' u 2:3 pt 6 lc 3 notitle, \
     'data/asymmetry_loM_points.txt' w lines lt 4 lc 3 notitle, \
     '' u 3:4 w lines lt 2 lc 3 notitle, \
     '' u 5:6 w lines lt 1 lw 3 lc 3 title 'mass<450GeV', \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 notitle
 
-plot [-0.04:0.04] [-0.007:0.007] \
+plot [-0.03:0.03] [-0.007:0.007] \
     'data/asymmetry_hiY.txt' u 2:3 pt 6 lc 1 notitle, \
     'data/asymmetry_hiY_points.txt' w lines lt 4 lc 1 notitle, \
     '' u 3:4 w lines lt 2 lc 1 notitle, \
     '' u 5:6 w lines lt 1 lw 3 lc 1 title 'tanhY>0.5', \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 notitle
 
-plot [-0.04:0.04] [-0.007:0.007] \
+plot [-0.03:0.03] [-0.007:0.007] \
     'data/asymmetry_loY.txt' u 2:3 pt 6 lc 4 notitle, \
     'data/asymmetry_loY_points.txt' w lines lt 4 lc 4 notitle, \
     '' u 3:4 w lines lt 2 lc 4 notitle, \
     '' u 5:6 w lines lt 1 lw 3 lc 4 title 'tanhY<0.5', \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 notitle
 
-plot [-0.04:0.04] [-0.007:0.007] \
+plot [-0.03:0.03] [-0.007:0.007] \
     'data/asymmetry_full_points.txt' u 5:6 w lines lt 1 lw 3 lc 7 title 'full', \
     'data/asymmetry_hiM_points.txt' u 5:6 w lines lt 1 lw 3 lc 5 title 'mass>450GeV', \
     'data/asymmetry_loM_points.txt' u 5:6 w lines lt 1 lw 3 lc 3 title 'mass<450GeV', \
@@ -51,7 +51,7 @@ plot [-0.04:0.04] [-0.007:0.007] \
     'data/asymmetry_loY_points.txt' u 5:6 w lines lt 1 lw 3 lc 4 title 'tanhY<0.5', \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 notitle
 
-plot [-0.04:0.04] [-0.007:0.007] \
+plot [-0.03:0.03] [-0.007:0.007] \
     'data/asymmetry_full_points.txt' w lines lt 4 lc 7 title 'full', \
     'data/asymmetry_hiM_points.txt' w lines lt 4 lc 5 title 'mass>450GeV', \
     'data/asymmetry_loM_points.txt' w lines lt 4 lc 3 title 'mass<450GeV', \
