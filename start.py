643f751e4eb7082ca921c6e695da7f7fa168056c
Burton Betchart <betchart@gmail.com>
2013-04-18 15:14:51 -0400

    changes for d_qq from rapidity

--- start.py
+++ start.py
@@ -90,17 +90,22 @@ class fit(object):
             for name,val in kwargs.items(): w.arg(name).setVal(val)
             return pll.getVal()
 
-        def vE(a) : return w.arg(a).getVal(), max(0.08, w.arg(a).getError())
+        errMin = {2:0.08, 1:0.005}[N]
+        def vE(a) : return w.arg(a).getVal(), max(errMin, w.arg(a).getError())
 
         muSig = [vE(a) for a in self.profileVars]
         if not self.pllPoints:
             print muSig
             self.pllPoints = [tuple(m + i*s for i, (m, s) in zip(signs, muSig))
                               for signs in sorted(set(combinations([0, 1, 0, -1, 0], N)))]
+            if N==1:
+                for i in range(len(self.pllPoints)):
+                    if self.pllPoints[i][0]<-1 : self.pllPoints[i] = ((muSig[0][0]-1)/2,)
+            print self.pllPoints
 
         points = [p + (pllEval(**dict(zip(self.profileVars, p))),) 
                   for p in self.pllPoints]
-        print zip(*points)[2]
+        print zip(*points)[-1]
 
         oneSigmaNLL = {1: 0.5, 2: 1.14}[N]
         if N == 1:
@@ -115,29 +120,39 @@ class fit(object):
         if self.doAsymm: 
             self.scales = np.array([w.arg(a).getVal() for a in ['Ac_y_ttqq', 'Ac_y_ttqg']])
             self.scalesPhi= [w.arg('Ac_phi_%s'%n) for n in ['ttqq','ttgg','ttag','ttqg','tt']]
-            self.fractionHats = [w.arg('f_%s_hat' % a).getVal() for a in ['gg','qg','qq','ag']]
+        self.fractionHats = [w.arg('f_%s_hat' % a).getVal() for a in ['gg','qg','qq','ag']]
 
         self.parb = parb
         self.muSig = muSig
         self.profPLL = pllEval(**dict(zip(self.profileVars, self.profVal)))
         self.fitPLL = pllEval(**dict(zip(self.profileVars,[m for m,_ in muSig])))
-        choosePLL = self.profErr[0,0] > 0 and self.profErr[1,1] > 0 and self.profPLL < self.fitPLL
+        choosePLL = ((N==3 and self.profErr[0,0] > 0 and self.profErr[1,1] > 0 
+                      and self.profPLL < self.fitPLL) or 
+                     (N==1 and self.profErr != None and self.profVal[0]>-1))
         self.best = self.profVal if choosePLL else [m for m,_ in muSig]
+        if N==1 and not choosePLL: self.profErr = muSig[0][1]
 
         if not self.quiet:
             print zip(*muSig)[0], self.fitPLL
             print self.profVal, self.profPLL
             print self.profErr
-            for item in ['d_qq','alphaL','d_xs_tt','d_xs_wj','factor_elqcd','factor_muqcd']:
+            for item in ['d_qq','d_xs_tt','d_xs_wj','factor_elqcd','factor_muqcd','alphaL'][:-1 if N==1 else None]:
                 print '\t',
                 w.arg(item).Print()
         return
 
     @classmethod
-    def fields(cls): return ('#label fqq.Ac_y_qq  fqg.Ac_y_qg  XX  XY  YY fhat_gg ' +
-                             'fhat_qg fhat_qq fhat_ag Ac_y_gg_hat Ac_y_qq_hat Ac_y_qg_hat fitstatus')
+    def fields(cls, doAsymm): 
+        if not doAsymm:
+            return ('#label d_qq error' +
+                    'fhat_gg fhat_qg fhat_qq fhat_ag status')
+        return ('#label fqq.Ac_y_qq  fqg.Ac_y_qg  XX  XY  YY fhat_gg ' +
+                'fhat_qg fhat_qq fhat_ag Ac_y_gg_hat Ac_y_qq_hat Ac_y_qg_hat fitstatus')
 
     def __str__(self):
+        if not self.doAsymm:
+            return '\t'.join(str(i) for i in [self.label] + list(self.best) + [self.profErr] + self.fractionHats + [self.status])
+
         return '\t'.join(str(i) for i in [self.label] +
                          list(self.best*self.scales) +
                          [self.profErr[0,0]*self.scales[0]**2,
@@ -150,7 +165,7 @@ class fit(object):
 class measurement(object):
     def __init__(self, label, signal, profile, R0_, hackZeroBins=False, alternateModel=False):
         write = open('data/' + '_'.join(label.split(',')) + '.txt', 'w')
-        print >> write, fit.fields()
+        print >> write, fit.fields('QueuedBin' in signal)
 
         self.central = fit(signal=signal, profileVars=profile, R0_=R0_,
                            hackZeroBins=hackZeroBins, alternateModel=alternateModel,
@@ -158,18 +173,22 @@ class measurement(object):
 
         print >> write, str(self.central)
 
-        para = self.central.parb.parametricEllipse(0.5)
-        angle = self.central.parb.major_angle()
-        points = [(x/w,y/w) for x,y,w in [para.dot([math.cos(angle+f*math.pi),
-                                                    math.sin(angle+f*math.pi),
-                                                    1])
-                                          for f in [0, 0.5, 0.75, 1.0, 1.5]]]
-        points.insert(0,self.central.parb.xymin)
-        self.central.pllPoints = points
-        
-        vars_ = ['d_qq' if not alternateModel else 'alphaL_mag',
-                'falphaL', 'falphaT','R_ag',
-                'd_xs_tt', 'd_xs_wj', 'factor_elqcd', 'factor_muqcd']
+        if 'QueuedBin' in signal:
+            para = self.central.parb.parametricEllipse(0.5)
+            angle = self.central.parb.major_angle()
+            points = [(x/w,y/w) for x,y,w in [para.dot([math.cos(angle+f*math.pi),
+                                                        math.sin(angle+f*math.pi),
+                                                        1])
+                                              for f in [0, 0.5, 0.75, 1.0, 1.5]]]
+            points.insert(0,self.central.parb.xymin)
+            self.central.pllPoints = points
+
+            vars_ = ['d_qq' if not alternateModel else 'alphaL_mag',
+                     'falphaL', 'falphaT','R_ag',
+                     'd_xs_tt', 'd_xs_wj', 'factor_elqcd', 'factor_muqcd']
+        else:
+            vars_ = ['d_qq','R_ag','d_xs_tt','d_xs_wj','factor_elqcd','factor_muqcd']
+
         defaults = dict([(v,self.central.model.w.arg(v).getVal()) for v in vars_])
         print '\n'.join(str(kv) for kv in defaults.items())
 
