bcc7143fcfda6817d61b4c9ac892504e567ceb3c
Burton Betchart <betchart@gmail.com>
2013-04-20 18:56:35 -0400

    plot MC uncertainty

--- plot.sh
+++ plot.sh
@@ -17,7 +17,8 @@ plot [-0.03:0.03] [-0.01:0.01] \
     '' u 3:4 w lines lt 2 lc 7 notitle, \
     '' u 5:6 w lines lt 1 lw 3 lc 7 title 'full', \
     '' u 7:8 w lines lt 1 lc 7 notitle, \
-    '' u 9:10 w lines lt 1 lw 3 lc 7 notitle, \
+    '' u 9:10 w lines lt 1 lw 4 lc 7 notitle, \
+    '' u 11:12 w lines lt 1 lc 2 notitle, \
     (x+ep >= qq && x <= qg + qq ? qq + qg - x : 1/0) lc 2 lt 2 notitle, \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 title 'POWHEG CT10'
 
@@ -27,7 +28,8 @@ plot [-0.03:0.03] [-0.01:0.01] \
     '' u 3:4 w lines lt 2 lc 5 notitle, \
     '' u 5:6 w lines lt 1 lw 3 lc 5 title 'mass>450GeV', \
     '' u 7:8 w lines lt 1 lc 7 notitle, \
-    '' u 9:10 w lines lt 1 lw 3 lc 5 notitle, \
+    '' u 9:10 w lines lt 1 lw 4 lc 5 notitle, \
+    '' u 11:12 w lines lt 1 lc 2 notitle, \
     (x+ep >= qq && x <= qg + qq ? qq + qg - x : 1/0) lc 2 lt 2 notitle, \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 title 'POWHEG CT10'
 
@@ -37,7 +39,8 @@ plot [-0.03:0.03] [-0.01:0.01] \
     '' u 3:4 w lines lt 2 lc 3 notitle, \
     '' u 5:6 w lines lt 1 lw 3 lc 3 title 'mass<450GeV', \
     '' u 7:8 w lines lt 1 lc 7 notitle, \
-    '' u 9:10 w lines lt 1 lw 3 lc 3 notitle, \
+    '' u 9:10 w lines lt 1 lw 4 lc 3 notitle, \
+    '' u 11:12 w lines lt 1 lc 2 notitle, \
     (x+ep >= qq && x <= qg + qq ? qq + qg - x : 1/0) lc 2 lt 2 notitle, \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 title 'POWHEG CT10'
 
@@ -45,9 +48,10 @@ plot [-0.03:0.03] [-0.01:0.01] \
     'data/asymmetry_hiY.txt' u 2:3 pt 6 lc 1 notitle, \
     'data/asymmetry_hiY_points.txt' w lines lt 4 lc 1 notitle, \
     '' u 3:4 w lines lt 2 lc 1 notitle, \
-    '' u 5:6 w lines lt 1 lw 3 lc 1 title 'tanhY>0.5', \
+    '' u 5:6 w lines lt 1 lw 3 lc 1 title '|tanh y|>0.5', \
     '' u 7:8 w lines lt 1 lc 7 notitle, \
-    '' u 9:10 w lines lt 1 lw 3 lc 1 notitle, \
+    '' u 9:10 w lines lt 1 lw 4 lc 1 notitle, \
+    '' u 11:12 w lines lt 1 lc 2 notitle, \
     (x+ep >= qq && x <= qg + qq ? qq + qg - x : 1/0) lc 2 lt 2 notitle, \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 title 'POWHEG CT10'
 
@@ -55,9 +59,10 @@ plot [-0.03:0.03] [-0.01:0.01] \
     'data/asymmetry_loY.txt' u 2:3 pt 6 lc 4 notitle, \
     'data/asymmetry_loY_points.txt' w lines lt 4 lc 4 notitle, \
     '' u 3:4 w lines lt 2 lc 4 notitle, \
-    '' u 5:6 w lines lt 1 lw 3 lc 4 title 'tanhY<0.5', \
+    '' u 5:6 w lines lt 1 lw 3 lc 4 title '|tanh y|<0.5', \
     '' u 7:8 w lines lt 1 lc 7 notitle, \
-    '' u 9:10 w lines lt 1 lw 3 lc 4 notitle, \
+    '' u 9:10 w lines lt 1 lw 4 lc 4 notitle, \
+    '' u 11:12 w lines lt 1 lc 2 notitle, \
     (x+ep >= qq && x <= qg + qq ? qq + qg - x : 1/0) lc 2 lt 2 notitle, \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 title 'POWHEG CT10'
 
@@ -73,9 +78,9 @@ plot [-0.03:0.03] [-0.01:0.01] \
     '' u 9:($10-2*eps-off) w lines lt 1 lw 7 lc 5 notitle, \
     'data/asymmetry_loM_points.txt' u 5:6 w lines lt 1 lw 3 lc 3 title 'mass<450GeV', \
     '' u 9:($10-3*eps-off) w lines lt 1 lw 7 lc 3 notitle, \
-    'data/asymmetry_hiY_points.txt' u 5:6 w lines lt 1 lw 3 lc 1 title 'tanhY>0.5', \
+    'data/asymmetry_hiY_points.txt' u 5:6 w lines lt 1 lw 3 lc 1 title '|tanh y|>0.5', \
     '' u 9:($10-4*eps-off) w lines lt 1 lw 7 lc 1 notitle, \
-    'data/asymmetry_loY_points.txt' u 5:6 w lines lt 1 lw 3 lc 4 title 'tanhY<0.5', \
+    'data/asymmetry_loY_points.txt' u 5:6 w lines lt 1 lw 3 lc 4 title '|tanh y|<0.5', \
     '' u 9:($10-4*eps-off) w lines lt 1 lw 7 lc 4 notitle, \
     (x+ep >= qq && x <= qg + qq ? qq + qg - x : 1/0) lc 2 lt 1 notitle, \
     "<echo '0.003953110899140473 0.0021230524809500123'" w points ls 1 lc 2 ps 3 lw 4 title 'POWHEG CT10'
