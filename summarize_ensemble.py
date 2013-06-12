0a26c59c790c4f3efb5a6254ccb491d369752ba1
Burton Betchart <betchart@gmail.com>
2013-06-12 02:36:24 -0400

    check 68% coverage: looks like 66% coverage, close enough

--- summarize_ensemble.py
+++ summarize_ensemble.py
@@ -27,16 +27,26 @@ if __name__ == '__main__':
     book = autoBook('book')
     names = ['efAqq','efAqg','pullqq','pullqg']
     limits = [(0,0.01),(0,0.003),(-3,3),(-3,3)]
+    truth = tuple(fA + [1.])
+    count=0
     for k in M:
         values = [abs(f) for f in deltas(k)] + pulls(k) 
-        print '\t'.join(['%+.5f'%i for i in values])
         for n,v,lim in zip(names,values,limits):
             book.fill(v,n,30,*lim)
-    
+        mean = M[k][:2]
+        sigmas2 = [[M[k][2], M[k][3]],
+                   [M[k][3], M[k][4]]]
+        el = ellipse(mean=mean,sigmas2=sigmas2)
+        level = np.dot(truth, el.matrix).dot(truth)
+        print '\t'.join(['%+.5f'%i for i in values+[level]])
+        if level<0: count+=1
+    print count
+
     c = r.TCanvas()
     c.Divide(2,2)
     for i,k in enumerate(names):
         c.cd(i+1)
         book[k].Fit('gaus')
         book[k].Draw()
+
     c.Print('.'.join(sys.argv[1].split('.')[:-1]+['pdf']))
