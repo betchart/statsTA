d15de576607a0abf30726ea3294f03c4ba2b9f99
Burton Betchart <betchart@gmail.com>
2013-07-02 18:16:05 -0400

    7 pages

--- topdf.py
+++ topdf.py
@@ -1,7 +1,7 @@
 import os
-
-for i in range(6):
+pages = 7
+for i in range(pages):
     os.system("psselect -p%d plot.ps plot%d.ps"%(i+1,i+1))
     os.system("epstopdf --autorotate=All plot%s.ps"%(i+1))
-os.system('pdftk %s cat output plot.pdf'%(' '.join(['plot%d.pdf'%(i+1) for i in range(6)])))
-os.system('rm %s'%(' '.join(['plot%d.*'%(i+1) for i in range(6)])))
+os.system('pdftk %s cat output plot.pdf'%(' '.join(['plot%d.pdf'%(i+1) for i in range(pages)])))
+os.system('rm %s'%(' '.join(['plot%d.*'%(i+1) for i in range(pages)])))
