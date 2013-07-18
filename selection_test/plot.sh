a903cdebf853a22872fe94e6aee92af4eddc29c8
Burton Betchart <betchart@gmail.com>
2013-07-18 02:57:03 -0400

    updates

--- selection_test/plot.sh
+++ selection_test/plot.sh
@@ -1,20 +1,20 @@
-set term postscript enhanced color 22 size 8in, 5in
+set term postscript enhanced color 22 size 8in, 8in
 set output 'plot.ps'
 set key bottom
 
-rab = 0.052
-aa = 0.0032
-ab = 0.0057
+rab = 0.1/0.9
+aA = 0.0032
+aB = 0.0057
 
-unf(f) = f * aa * (rab + (1-aa*ab+(ab/aa-1)/f) / (1-aa**2)) / (rab + (1-aa*ab+f*aa*(ab-aa)) / (1-aa**2))
+aE(aG,AA,AB) = (aG*(1-AA**2)*rab + aG*(1-AA*AB) + (AB-AA)) / ( (1-AA**2)*rab + (1-AA*aB) + aG*(AB-AA))
 
 L(RAB) = RAB / (1+RAB)
 
-alpha(f) = f * (L(rab)*aa + (1-L(rab))*ab)
+aD(aG,AA,AB) = aG * (L(rab)*AA + (1-L(rab))*AB) / AA
 
 set grid
-set xlabel 'F'
-set ylabel 'A@_c^y'
+set xlabel 'A@_c^y (%) in lepton+jets selection '
+set ylabel 'extrapolated total A@_c^y (%)'
 
-
-plot [-3:3] alpha(x) title 'true asymmetry',unf(x) title 'calculation via unfolding' lc 3, "<echo '1 0.0056'" title 'POWHEG' lc 2
+lim = 1
+plot [-lim:lim] 100*aD(x/100, aA, aB) title 'POWHEG 2-bin templates', 100*aE(x/100, aA, aB) title 'POWHEG 2-bin unfolding' lc 3, "<echo '0.32 0.56'" title 'POWHEG' lc 2, x title 'bisector'
