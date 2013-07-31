e429ad0e3ccc95a3ecbac2b3375213046c8cf2cb
Burton Betchart <betchart@gmail.com>
2013-07-31 18:11:31 -0400

    almost batch

--- start.py
+++ start.py
@@ -7,6 +7,13 @@ from ensembles import ensemble_specs
 from measurement import measurement
 from options import opts
 from inputs import channel_data
+import os
+
+def chunk(L,n):
+    return [L[i:i+n] for i in range(0,len(L), n)] if L else []
+
+def chunktuple(T,n):
+    return [(i,min(i+n,T[1])) for i in range(T[0],T[1], n)] if T else []
 
 if __name__ == '__main__':
     r.gROOT.SetBatch(1)
@@ -38,16 +45,23 @@ if __name__ == '__main__':
     templates = ((0,0) if not options.templates else tuple(int(i) for i in options.templates.split(':')))
     
     if options.batch:
-        chunk = 10
-        pass
+        chunksize = 10
+        stack = []
+        for part in partitions:
+            syschunks = chunk(systs, chunksize)
+            enschunks = chunktuple(ensSlice, chunksize)
+            tmpchunks = chunktuple(templates, chunksize)
+            if syschunks: stack.extend(["./start.py --partition %s --systematics %s"%(part, ','.join(s)) for s in syschunks])
+            if templates: stack.extend(["./start.py --partition %s --templates %d:%d"%((part,)+t) for t in tmpchunks])
+            if enschunks: stack.extend(["./start.py --partition %s --ensembles %s --ensSlice %d:%d"%((part, e)+s) for s in enschunks for e in ensembles])
     else:
         for part in partitions:
             for tID in ([None] if templates[0]==templates[1] else 
                         range(channel_data.nTemplates)[slice(*templates)]):
                 mp = systematics.measurement_pars(partition=part)
                 mp.update({'doVis':options.visualize,
-                           'evalSystematics':systs,
-                           'ensembles':ensembles,
+                           'evalSystematics':systs if tID == None else [],
+                           'ensembles':ensembles if tID == None else [],
                            'ensSlice':ensSlice,
                            'templateID':tID})
                 print mp
