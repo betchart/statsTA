f79f18b081ca859292e59982a58f7501d4b3328d
Burton Betchart <betchart@gmail.com>
2013-08-02 19:47:25 +0100

    batched

--- batch.py
+++ batch.py
@@ -1,5 +1,6 @@
 import os
 import utils
+import tempfile
 
 class batch(object):
     def __init__(self,cmds, nCores=4, site=None):
@@ -7,7 +8,21 @@ class batch(object):
         self.cmds = cmds
 
         if site == 'ic':
-            pass
+            localdir =  '/'.join(__file__.split('/')[:-1])
+            prelude = '''
+export SCRAM_ARCH=slc5_amd64_gcc472
+source /vols/cms/grid/setup.sh
+cd /home/hep/bbetchar/work/CMSSW_6_2_0_patch1/src
+eval `scram runtime -sh`
+cd %s
+'''%localdir
+            def sub(iJob,cmd):
+                with tempfile.NamedTemporaryFile('w',prefix='job%04d_'%iJob,suffix='.sh') as tmp:
+                    print>>tmp, prelude
+                    print>>tmp, cmd
+                    tmp.flush()
+                    os.system('./ic_cmsSub.sh %s'%tmp.name)
+            utils.operateOnListUsingQueue(self.nCores, utils.qWorker(sub), enumerate(self.cmds))
         else:
             self.default()
 
